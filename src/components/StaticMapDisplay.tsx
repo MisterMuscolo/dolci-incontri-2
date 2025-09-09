313f4d0d26ca58a50548dd9f161e429ee5e61cdd
[dyad] <gamberogrosso90@hotmail.com>
2025-09-09 22:35:24 +0200

    [dyad] Addressing Stadiamaps API key restriction for static maps - wrote 1 file(s)

--- src/components/StaticMapDisplay.tsx
+++ src/components/StaticMapDisplay.tsx
@@ -39,8 +39,8 @@ export const StaticMapDisplay = ({
     );
   }
 
-  // L'URL corretto per l'API delle mappe statiche di Stadiamaps dovrebbe includere /api/static/v1/
-  const mapUrl = `https://tiles.stadiamaps.com/api/static/v1/osm_bright/${longitude},${latitude},${zoom}/${width}x${height}.png?markers=lonlat:${longitude},${latitude}|color:ff0000|label:A&api_key=${STADIAMAPS_API_KEY}`;
+  // L'URL corretto per l'API delle mappe statiche di Stadiamaps
+  const mapUrl = `https://tiles.stadiamaps.com/styles/osm_bright/static/${longitude},${latitude},${zoom}/${width}x${height}.png?markers=lonlat:${longitude},${latitude}|color:ff0000|label:A&api_key=${STADIAMAPS_API_KEY}`;
   
   console.log("Stadiamaps Map URL:", mapUrl);
 
