b772b3f968b8636bf5700813105c08fc41962740
[dyad] <gamberogrosso90@hotmail.com>
2025-09-09 14:18:03 +0200

    [dyad] Configurazione di Vite per risolvere i problemi di rendering della mappa. - wrote 1 file(s)

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,7 @@ export default defineConfig(() => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  optimizeDeps: {
+    exclude: ['react-leaflet', 'leaflet'], // Escludi queste librerie dalla pre-ottimizzazione
+  },
 }));
