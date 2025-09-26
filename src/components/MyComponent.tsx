2e6ef6fff1a190f79c18085d1a0708b3efae56d5
[dyad] <mistermuscolo90@hotmail.com>
2025-09-26 20:43:26 +0200

    [dyad] wrote 2 file(s)

---
+++ src/components/MyComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>Ciao, mondo!</h1>
+      <p>Questo è un nuovo paragrafo aggiunto al componente.</p>
+    </div>
+  );
+};
+
+export default MyComponent;
---
+++ src/utils/helpers.js
@@ -0,0 +1,7 @@
+function greet(name) {
+  return `Ciao, ${name}!`;
+}
+
+module.exports = {
+  greet,
+};
